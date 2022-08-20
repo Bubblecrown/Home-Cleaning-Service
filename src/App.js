7b6bbccf25a6f27d9b5b8526664e81efd8405888
Bubblecrown <suchanya.namwong1@gmail.com>
2022-08-21 00:40:23 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
+import GlobalStyles from './globalStyles';
+
+// pages
+import HomePage from './pages/HomePage';
+import PricingPage from './pages/PricingPage';
+import SingupPage from './pages/SingupPage';
+// ens pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyles></GlobalStyles>
+      <Switch>
+        <Route path='/Home' exact component={HomePage}/>
+        <Route path='/Signup' exact component={SingupPage}/>
+        <Route path='/Pricing' exact component={PricingPage}/>
+      </Switch>
+    </Router>
   );
 }
 
