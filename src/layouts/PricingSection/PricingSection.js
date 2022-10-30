b3a537a7164dc0b965a07ddd4098087b32a2987c
Bubblecrown <suchanya.namwong1@gmail.com>
2022-10-30 21:27:31 +0700

    finish display data in price section

--- src/layouts/PricingSection/PricingSection.js
+++ src/layouts/PricingSection/PricingSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Section, Row } from "../../globalStyles";
 import TitleSection from "../../components/TitleComponent/TitleSection";
-import { PriceData, PriceService, TierData } from "../../data/PricingData";
+import { PriceData, TierData } from "../../data/PricingData";
 // import Card from "../../components/Card/Card";
 // import { CardPlan } from "../../components/Card/CardStyle";
 import {
@@ -31,28 +31,16 @@ const PricingSection = () => {
                 <Price>{item.price}</Price>
                 <PriceTitle>monthly</PriceTitle>
               </PriceCircle>
-              <PlanList>
-                {PriceService.map((service, index) => (
-                  <>
-                    <ListIcon key={index}>
-                      {(item.window ||
-                        item.carpet ||
-                        item.furniture ||
-                        item.car ||
-                        item.bathroom) && <GoCheck />}
+                {item.serviceList.map((list, index) => (
+                  <PlanList key={index}>
+                    <ListIcon >
+                      {list.service ? <GoCheck /> : <GrFormClose />}
                     </ListIcon>
-                    <ServiceList key={index}>{service}</ServiceList>
-                  </>
+                    <ServiceList>{list.textList}</ServiceList>
+                  </PlanList>
                 ))}
-              </PlanList>
             </CardPlan>
           ))}
-          <ListIcon>
-            <GoCheck />
-          </ListIcon>
-          <ServiceList>
-          window cleaning
-          </ServiceList>
           {/* <Card {...TierData}/> */}
         </Row>
       </Container>
