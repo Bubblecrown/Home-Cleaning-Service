50beef172de5cffb173bce04a952fa3c897169ca
Bubblecrown <suchanya.namwong1@gmail.com>
2022-11-02 19:54:24 +0700

    finish address - Contact section

--- src/layouts/ContactSection/ContactStyle.js
+++ src/layouts/ContactSection/ContactStyle.js
@@ -8,15 +8,26 @@ import {
   Secondary,
 } from "../../globalStyles";
 
-export const AddressTitle = styled.h1``;
+export const AddressTitle = styled.h1`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
 
-export const AddressDetail = styled.h1``;
+export const AddressDetail = styled.p`
+  text-transform: capitalize;
+  font-size: 1.1rem;
+  color: ${Secondary};
+`;
 
 export const ContactColumn = styled(Column)`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   row-gap: 20px;
+  max-width: 42%;
+  flex-basis: 42%;
 `;
 
 export const FormColumn = styled(Column)``;
@@ -24,6 +35,8 @@ export const FormColumn = styled(Column)``;
 export const ContactRow = styled(Row)`
   &:not(:first-child) {
     border-top: 2px solid ${LineColor};
+    margin-top: 40px;
+    padding-top: 20px;
   }
 `;
 
